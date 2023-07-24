a0331f1bbee938de58357ff6388d18ee6e96421a
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-07-25 05:14:00 +0530

    Added : Code for delete int , char , and double array

---
+++ DeleteArray.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProblem
+{
+    internal class DeleteArray
+    {
+        public void delIntArray(int[] numbers, int position)
+        {
+            Console.WriteLine("Before deleting the element the int array is");
+            foreach (int num in numbers)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+            // This for loop for deleting the element
+            for (int i = position; i < numbers.Length - 1; i++)
+            {
+                numbers[i] = numbers[i + 1];
+            }
+            Console.WriteLine("After deleting the element");
+            foreach (int num in numbers)
+            {
+                Console.Write(num + " ");
+            }
+
+        }
+        public void delDoubleArray(double[] doubles, int position)
+        {
+            Console.WriteLine("Before deleting the element the double array is");
+            foreach (double du in doubles)
+            {
+                Console.Write(du + " ");
+            }
+            Console.WriteLine();
+            // This for loop for deleting the element
+            for (int i = position; i < doubles.Length - 1; i++)
+            {
+                doubles[i] = doubles[i + 1];
+            }
+            Console.WriteLine("After deleting the element");
+            foreach (double du in doubles)
+            {
+                Console.Write(du + " ");
+            }
+        }
+        public void delCharArray(char[] chars, int position)
+        {
+            Console.WriteLine("Before deleting the element the char array is");
+            foreach (char ch in chars)
+            {
+                Console.Write(ch + " ");
+            }
+            Console.WriteLine();
+            // This for loop for deleting the element
+            for (int i = position; i < chars.Length - 1; i++)
+            {
+                chars[i] = chars[i + 1];
+            }
+            Console.WriteLine("After deleting the element");
+            foreach (char ch in chars)
+            {
+                Console.Write(ch + " ");
+            }
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Generics Problem");
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            double[] doubles = { 1.1, 2.3, 4.5, 5.6 };
+            char[] chars = { 'a', 'e', 'i', 'o', 'u' };
+            DeleteArray deleteArray = new DeleteArray();
+            deleteArray.delIntArray(numbers, 2);
+            Console.WriteLine();
+            deleteArray.delDoubleArray(doubles, 1);
+            Console.WriteLine();
+            deleteArray.delCharArray(chars, 3);
+            Console.WriteLine();
         }
     }
 }
